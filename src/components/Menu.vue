0d8b1271d21d092c98da905ac74f157fff9ec3c9
Jeremy Primat <valk.jeremy@gmail.com>
2019-01-10 23:23:07 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,12 +1,92 @@
 <template>
-  <div></div>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-toolbar color="teal" dark>
+
+          <v-toolbar-title>Carte du restaurant</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-list>
+          <v-list-group
+            v-for="item in items"
+            v-model="item.active"
+            :key="item.title"
+            :prepend-icon="item.action"
+            no-action
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile
+              v-for="subItem in item.items"
+              :key="subItem.strMeal"
+              @click=""
+            >
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ subItem.strMeal }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+
+              <v-list-tile-action>
+                <v-icon>{{ subItem.action }}</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-group>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import Utils from "../Utils.js";
 export default {
   data() {
-    return {};
+    return {
+      promises : [],
+      items: [
+          {
+            action: 'restaurant',
+            title: 'Nos Fabuleuses entrées',
+            active: true,            
+            items: [
+              {strMeal : "Soupe de poisson fait maison !"},
+              {strMeal : "Morrue sêchées!"},
+              {strMeal : "Salade César !"},
+              {price : "Toutes nos entrées sont à 9 euros"},
+
+            ]
+          },
+          {
+            action: 'restaurant',
+            title: 'Les plats du chef :D',
+            items: [
+              {strMeal : "Teryaki poulet !"},
+              {strMeal : "Agneau sauté à la pékinoise!"},
+              {strMeal : "Couscous aux lardons !"},
+              {price : "Tous nos plats sont à 11 euros"},
+              
+            ]
+          },
+          {
+            action: 'restaurant',
+            title: 'Nos desserts préparés avec amour',
+            items: [
+              {strMeal : "Crème brulée !"},
+              {strMeal : "Frommage blanc aux fruits rouges de saison !"},
+              {strMeal : "Boules Cocos !"},
+              {price : "Tous nos desserts sont à 4 euros"},
+            ]
+          },
+        ]
+    };
   },
   mounted() {
     this.getMenus();
@@ -19,6 +99,7 @@ export default {
           var starter = "Starter";
           var desert = "Desert";
           let promises = [];
+
 
           promises.push(this.API.getMenuFromCategory(starter));
 
